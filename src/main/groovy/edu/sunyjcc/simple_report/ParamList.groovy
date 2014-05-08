98b750615665dae02c229645011745e0edf10a8d
Ed Cole <edcole@mail.sunyjcc.edu>
2014-05-08 10:53:48 -0400

    Create ParamList.groovy to represent parameter lists.

---
+++ src/main/groovy/edu/sunyjcc/simple_report/ParamList.groovy
@@ -0,0 +1,10 @@
+package edu.sunyjcc.simple_report;
+
+import java.util.*
+
+/** An ordered list of parameters that can be used with the SimpleReports 
+ *  system.
+ */
+public class ParamList extends ArrayList {
+  
+}
