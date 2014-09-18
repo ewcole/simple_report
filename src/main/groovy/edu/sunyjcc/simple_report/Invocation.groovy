9c7cd95f0d4fb12ebe090c7daffbe6a526393ed5
Ed Cole <edcole@mail.sunyjcc.edu>
2014-09-18 11:29:00 -0400

    Create Invocation and InvocationFactory classes.

---
+++ src/main/groovy/edu/sunyjcc/simple_report/Invocation.groovy
@@ -0,0 +1,14 @@
+package edu.sunyjcc.simple_report
+
+/** This class Client applications will get objects of this class from a 
+ *  SimpleReportInvocationFactory object.
+ *  
+ */
+public class Invocation {
+  InvocationFactory factory
+  String            type
+  String            name
+  public Invocation(factory, type, name) {
+
+  }
+}
---
+++ src/main/groovy/edu/sunyjcc/simple_report/InvocationFactory.groovy
@@ -0,0 +1,12 @@
+package edu.sunyjcc.simple_report
+
+/** Client applications will normally interact only with objects of this class
+ *  and the SimpleReportInvocation class, which this factory builds.
+ *  
+ */
+public class InvocationFactory {
+  
+  Invocation newInvocation(String type, String name) {
+    
+  }
+}
