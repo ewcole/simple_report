030b43e1b5faf4c4135d06cdee4dfa8955d85d3c
Ed Cole <edcole@mail.sunyjcc.edu>
2014-10-13 15:23:00 -0400

    working_files.el - Add "Source" and "Test" header lines.

--- working_files.el
+++ working_files.el
@@ -6,6 +6,7 @@
     )
   )
 
+;; Create a document, working_files.org, with links to all Groovy files.
 (let ((work-buffer-name "working_files.org"))
   (find-file work-buffer-name)
   (erase-buffer)
@@ -14,5 +15,17 @@
   (while (re-search-forward "\\(.*\\\\\\([A-Za-z0-9]+\.groovy\\)\\)" nil t)
     (insert (concat "- [[file:" (get-re-match 1) "][" (get-re-match 2) "]]"))
     (delete-region (match-beginning 0) (match-end 0)))
+  (goto-char 0)
+  ;; find the first \src\main\ line and create Source header
+  (progn 
+    (search-forward "\\src\\main\\" nil t)
+    (search-backward "\n")
+    (insert "\n* Source"))
+  ;; find the first \src\test\ line and create Test header
+  (progn 
+    (search-forward "\\src\\test\\" nil t)
+    (search-backward "\n")
+    (insert "\n* Test"))
+  ;; Save this file.
   (save-current-buffer)
 )
