d377cf4f531e0fa10929df51ebe3d04f680768b8
Ed Cole <edcole@mail.sunyjcc.edu>
2014-11-06 11:18:10 -0500

    Start fleshing out Invocation.groovy

--- src/main/groovy/edu/sunyjcc/simple_report/Invocation.groovy
+++ src/main/groovy/edu/sunyjcc/simple_report/Invocation.groovy
@@ -1,14 +1,38 @@
 package edu.sunyjcc.simple_report
 
-/** This class Client applications will get objects of this class from a 
- *  SimpleReportInvocationFactory object.
- *  
+/**
+ *
  */
-public class Invocation {
-  InvocationFactory factory
-  String            type
+public class Invocation implements Exportable {
+  /** Our source for all objects */
+  ReportObjectFactory  factory
+  /** A string representing the type of object we are trying to run. */
+  String            reportObjectType
+  /** The name of the object we are trying to run. */
   String            name
-  public Invocation(factory, type, name) {
+  /** A unique name (within the factory) that identifies this invocation. */
+  String id;
+  /** A parameter form to hold our values */
+  ParamFormValue    params;
+  boolean isValid = false;
 
+  /** Create a new invocation object with the type and name given.  Generally,
+   *  You would not call this directly. */
+  public Invocation(ReportObjectFactory factory,
+                    String reportObjectType,
+                    String name) {
+    this.factory          = factory;
+    this.reportObjectType = reportObjectType;
+    this.name             = name;
+  }
+
+
+  /** Return a HashMap with info about this Invocation. */
+  def export() {
+    [type: reportObjectType,
+     name: name,
+     id:   id,
+     isValid: isValid,
+     params: params.export()]
   }
 }
