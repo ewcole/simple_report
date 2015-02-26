7d0b8a1ae8423d5b38029a539b5ae6c1ef8a5bee
Ed Cole <edcole@mail.sunyjcc.edu>
2015-02-26 17:02:48 -0500

    Create BuildException class

---
+++ src/main/groovy/edu/sunyjcc/simple_report/BuildException.groovy
@@ -0,0 +1,19 @@
+package edu.sunyjcc.simple_report
+
+public class BuildExeption extends Exception {
+
+  /** One-argument constructor that sends the error message.
+   *  @param message The error message
+   */
+  public BuildException(String message) {
+    super(message);
+  }
+  // /** */
+  // def parent
+  // /** Useful and informative information about this error. */
+  // def data
+
+  /** The source code for the object being built, if available.  */
+  String source;
+
+}
