690d50000a721db435ba12318780fcc18568cc8d
Ed Cole <edcole@mail.sunyjcc.edu>
2015-11-11 15:50:33 -0500

    Update documentation in Client.groovy and ClosureClient.groovy

--- src/main/groovy/edu/sunyjcc/simple_report/ClosureClient.groovy
+++ src/main/groovy/edu/sunyjcc/simple_report/ClosureClient.groovy
@@ -6,6 +6,7 @@ public class ClosureClient extends Client {
 
   /** A closure that shows one parameter */
   Closure showParamClosure;
+
   /** A closure that shows a parameter form */
   Closure showParamFormClosure;
 
