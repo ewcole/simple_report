73a769ec682e57248e8c45f49e3f717ea8d57788
Ed Cole <edcole@mail.sunyjcc.edu>
2015-12-15 11:53:17 -0500

    1.2.3: Add isBinary property to OutputFormat

--- src/main/groovy/edu/sunyjcc/simple_report/OutputFormat.groovy
+++ src/main/groovy/edu/sunyjcc/simple_report/OutputFormat.groovy
@@ -2,30 +2,34 @@ package edu.sunyjcc.simple_report
 
 enum OutputFormat {
   
-    pdf("PDF", "Adobe Acrobat", "application/pdf"),
-    html("HTML", "HTML Web Page", "text/html"),
-    xml("XML", "XML Data", "text/xml"),
-    csv("CSV", "Comma-Separated Values (for spreadsheets)", "application/csv"),
-    xls("XLS", "Excel 97-2007", "application/vnd.ms-excel"),
-    rtf("RTF", "Rich Text", "application/rtf"),
-    text("TEXT", "Plain Text", "text/plain", "txt"),
-    odt("ODT", "Open Office Word", "application/odt"),
-    ods("ODS", "Open Office Spreadsheet", "application/ods"),
-    docx("DOCX", "Microsoft Word", "application/msword"),
-    xlsx("XLSX", "Microsoft Excel", "application/vnd.ms-excel"),
-    pptx("PPTX", "Microsoft Powerpoint", "application/vnd.ms-powerpoint"),
+    pdf("PDF", "Adobe Acrobat", true, "application/pdf"),
+    html("HTML", "HTML Web Page", false, "text/html"),
+    xml("XML", "XML Data", false, "text/xml"),
+    csv("CSV", "CSV (for spreadsheets)", false, "application/csv"),
+    xls("XLS", "Excel 97-2007", true, "application/vnd.ms-excel"),
+    rtf("RTF", "Rich Text", true, "application/rtf"),
+    text("TEXT", "Plain Text", false, "text/plain", "txt"),
+    odt("ODT", "Open Office Word", true, "application/odt"),
+    ods("ODS", "Open Office Spreadsheet", true, "application/ods"),
+    docx("DOCX", "Microsoft Word", true, "application/msword"),
+    xlsx("XLSX", "Microsoft Excel", true, "application/vnd.ms-excel"),
+    pptx("PPTX", "Microsoft Powerpoint", true, "application/vnd.ms-powerpoint"),
     // Not supported by Jasper Reports.
-    json("JSON", "JSON Data", "application/json"),
+    json("JSON", "JSON Data", false, "application/json"),
 
-    OutputFormat(String code, String desc, String mimeType, String extension = '') {
+    OutputFormat(String code, String desc, 
+                 boolean isBinary,
+                 String mimeType, String extension = '') {
       this.code = code
       this.desc = desc
+      this.isBinary = isBinary
       this.mimeType = mimeType
       this.extension = (extension?.size())?extension:(code.toLowerCase());
     }
 
     String code;
     String desc;
+    boolean isBinary;
     String mimeType;
     String extension
 
