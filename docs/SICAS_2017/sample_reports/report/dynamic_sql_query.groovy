837baa326112bba1a33959822c87b202f5b4d3c4
Ed Cole <edcole@mail.sunyjcc.edu>
2017-08-14 18:01:55 -0400

    Flesh in report examples

--- docs/SICAS_2017/sample_reports/report/dynamic_sql_query.groovy
+++ docs/SICAS_2017/sample_reports/report/dynamic_sql_query.groovy
@@ -11,12 +11,23 @@ report(title: "Grade Totals") {
         'default': 'F') {
     list_of_values values: ('A'..'F').collect{[it, it]}
   }
+
+  // The data source for this report is a dynamic SQL query.
+  //     The dynamic_sql method takes a closure as an argument.
+  //     This one will use the value of the mid_or_final parameter
+  //     to choose which grade column to report on, and it will
+  //     also use the grade parameter to set one of the column names.
   dynamic_sql sql_closure: {
     params ->
-      // Use the selected grade column
+      
+      // Get the parameters from the closure arguments.
       String mid_or_final = params.mid_or_final;
-      String grade = params.grade;
+      String grade = ('A'..'F').contains(params.grade)?params.grade:'F';
+      
+      // Set the grade column 
       String grade_column = (mid_or_final == 'M')?'mid_grade':'final_grade'
+
+      // Return an SQL query.
       """
        select semester,
               count(${grade_column}) as ${grade_column},
