a8a8972e47c3e9a7f195d1f556cf2b514782d7a4
Ed Cole <EdwardWCole@gmail.com>
2021-02-19 09:36:09 -0500

    Correct handling of system parameters in Jasper reports

--- src/main/groovy/edu/sunyjcc/simple_report/SimpleJobEngine.groovy
+++ src/main/groovy/edu/sunyjcc/simple_report/SimpleJobEngine.groovy
@@ -1,5 +1,7 @@
 package edu.sunyjcc.simple_report
 
+import groovy.xml.*;
+
 /** A wrapper for a job engine closure that allows it to be added to a SimpleJob
  *  by the SimpleReportBuilder
  */
@@ -10,13 +12,54 @@ public class SimpleJobEngine implements Buildable {
   
   /** Provide a description for this object. */
   String getBuildDocHtml() {
-    ("This creates an engine that can perform actions with side effects.")
+    def s = new StringWriter()
+    def m = new MarkupBuilder(s);
+    m.useDoubleQuotes = true
+    m.div(class: "simple-job-engine-base-doc") {
+      p("This creates an engine that can perform actions with side effects.");
+    };
+    return s.toString();
   }
+
+
+    /** This code generates documentation about the closure 
+   *  needed to generate the data for a ClosureQueryEngine.  */
+  static String getQueryClosureDocs() {
+    def s = new StringWriter()
+    def m = new MarkupBuilder(s);
+    m.useDoubleQuotes = true
+    m.div(class: "closure-engine-doc") {
+      p {
+        mkp.yield """This is a closure that executes the action the job is 
+           supposed to do."""
+      }
+      "h3"("Predefined Properties");
+      dl(class: "properties") {
+        dt("sql"); 
+        dd("A groovy.sql.Sql database connection");
+        // paramFormValue
+        dt("params")
+        dd{
+          mkp.yield("A ")
+          a(href: "file://x:doc/groovydoc/simple_report/edu/sunyjcc/simple_report/ParamFormValue.html",
+            "ParamFormValue")
+          mkp.yield " object, representing the parameters given by the user."
+        }
+        dt("markupBuilder");
+        dd {
+          mkp.yield("A groovy.xml.MarkupBuilder that writes to the response body text.");
+          
+        }
+      }
+    }
+    return s.toString()
+  }
+
 
   /** List the different options you can pass as parameters to the builder 
    *  method call for this class. */
   LinkedHashMap getBuildOptions() {
-    [closure: [desc: "A closure to execute the action."]]
+    [closure: [desc: getQueryClosureDocs()]]
   }
 
   Closure closure;
